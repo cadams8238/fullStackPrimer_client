42d6e71a8d10cdf009890e511d1b1283dc4235dd
Courtney Adams <cadams8238@gmail.com>
2018-06-29 17:56:26 -0700

    connected cheeseList component to redux store

--- src/App.js
+++ src/App.js
@@ -2,21 +2,12 @@ import React, { Component } from 'react';
 import CheeseList from './components/cheeseList'
 
 class App extends Component {
-    constructor(props) {
-        super(props)
-
-        this.cheeses = [
-            "Bath Blue",
-            "Barkham Blue",
-            "Buxton Blue"
-        ];
-    }
 
 
 
     render() {
         return (
-          <CheeseList cheeses={this.cheeses}/>
+          <CheeseList />
         );
     }
 }
--- src/components/cheeseList.js
+++ src/components/cheeseList.js
@@ -1,15 +1,35 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchCheeses } from '../actions/cheeseAction';
 
-export default function CheeseList(props) {
-    const cheeses = props.cheeses.map((cheese, index) => (
-        <li key={index}>
-            {cheese}
-        </li>
-    ));
+export class CheeseList extends React.Component {
 
-    return (
-        <ul>
-            {cheeses}
-        </ul>
-    );
+    componentDidMount() {
+        this.props.dispatch(fetchCheeses())
+    }
+
+    renderCheeses() {
+        return (this.props.cheeses.map((cheese, index) => (
+            <li key={index}>
+                {cheese}
+            </li>
+        )));
+    }
+
+    render() {
+        return (
+            <ul>
+                {this.renderCheeses()}
+            </ul>
+        );
+    }
+
 }
+
+const mapStateToProps = state => ({
+    // console.log(state.cheeseReducer.cheeses)
+    cheeses: state.cheeseReducer.cheeses
+});
+
+
+export default connect(mapStateToProps)(CheeseList);
